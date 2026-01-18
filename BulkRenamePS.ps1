b40c0b1d4ca5da632388ead970b1444d23df501c
Aleksander Hoff <aleksander.hoff06@gmail.com>
2026-01-18 13:02:21 +0100

    Path parameter og default Path Fixes #1 Done

--- BulkRenamePS.ps1
+++ BulkRenamePS.ps1
@@ -1,1 +1,18 @@
+# ============================================
+# Skript: BulkRenamePS.ps1
+# Formål: Gir nytt navn i bulk basert på mønster spesifisert
+# Forfatter: Aleksander Hoff (GH:@AVEX41)
+# Dato: Later
+# ============================================
 
+# Gathering or populating the Path variable
+param(
+    [Parameter(Mandatory = $false)]
+    [ValidateNotNullOrEmpty()]
+    [string]$Path = (Get-Location).Path
+)
+
+# Checking if the path is valid, if not: 
+# the error action is to stop and inform user
+try { $ResolvedPath = (Resolve-Path -LiteralPath $Path -ErrorAction Stop).Path } 
+catch { Write-Error "Invalid path: $Path"; exit 1 }
